83c43c635611a0139d346724917f00225a5c06a5
User <user@apeg.home>
2017-03-10 17:15:51 -0500

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,32 @@ const webpackValidator = require('webpack-validator')
 
 module.exports = env => {
     const {ifProd, ifNotProd} = getIfUtils(env)
-    const config = webpackValidator({})
+    const config = webpackValidator({
+      context: resolve('js'),
+      entry: '',
+      output: {
+        filename: '',
+        path: resolve('dist'),
+        publicPath: '/dist/',
+        pathinfo: ifNotProd()
+      },
+      devtool: ifProd('source-map', 'eval'),
+      module: {
+        preloaders: [
+          {
+            test: /\.jsx?$/,
+            loader: ['eslint-loader'],
+            exclude: /node_modules/
+          }
+        ],
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            loader: ['babel-loader'],
+            exclude: /node_modules/
+          }
+        ]
+      }
+    })
 }
 
